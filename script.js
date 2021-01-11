4653998efad3ff84327f1651a75232ec49ee5f86
iGreenapple <72871903+iGreenapple@users.noreply.github.com>
2021-01-11 17:56:29 +0100

    11_1-17_55

--- script.js
+++ script.js
@@ -8,35 +8,39 @@ const URL_OSM = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
 const OSM = L.tileLayer(URL_OSM, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 17,
-    minZoom: 10
+    maxZoom: 16,
+    minZoom: 9
 });
-const CARTO = L.tileLayer(CARTO_URL);
+const CARTO = L.tileLayer(CARTO_URL, {
+    maxZoom: 16,
+    minZoom: 9
+});
 
 // const HK = L.marker([50.208568, 15.831504]);
 // HK.bindPopup("Tady je Hradec");
 // const CIRCLE = L.polyline([[50.203085, 15.822044], [50.20790846247731, 15.818724314811007], [50.215324304591455, 15.822315081095866], [50.21744085888751, 15.829658747939503], [50.215693252024046, 15.842282572025091], [50.21128511467276, 15.847411000548513], [50.205776354759806, 15.848206739218869], [50.20204066890915, 15.842112760370972], [50.20095903905872, 15.834553847280281], [50.203085, 15.822044]], {
 //     color: '#0f3057',
 //     weight: 5
-
 // });
 
-const Ortofoto_URL = "https://geoportal.cuzk.cz/WMS_ORTOFOTO_PUB/WMService.aspx"
-const Ortofoto = L.tileLayer.wms(Ortofoto_URL, {
-    layers: 'Ortofoto',
-    format: 'image/gif',
-    transparent: true
-});
-
 // const VECTOR = L.layerGroup([HK, CIRCLE])
-const baseLayers = {
-    'Carto': CARTO,
-    'OpenStreetMap': OSM,
-    'Ortofoto': Ortofoto,
-};
 // const overlays = {
 //     'Vector': VECTOR,
 // }
+
+const Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    maxZoom: 15,
+    minZoom: 9
+});
+
+
+const baseLayers = {
+    'Carto': CARTO,
+    'OpenStreetMap': OSM,
+    'Satelitní': Esri_WorldImagery,
+};
+
 const LAYERS = L.control.layers(baseLayers);
 
 const WATER = 'https://zelda.sci.muni.cz/geoserver/webovka/ows?' +
@@ -68,15 +72,38 @@ fetch(WATER)
                 return content;
             });
 		
-        LAYERS.addOverlay(WATER_CLUSTER, "water HK");
+        LAYERS.addOverlay(WATER_CLUSTER, "Vodní plochy - INFO");
+    });
+
+const WATER_POLYGON = 'https://zelda.sci.muni.cz/geoserver/webovka/ows?' +
+'service=WFS&' + 
+'version=2.0.0&' + 
+'request=GetFeature&' +
+'typename=webovka%3ANadrze_selected_HK&' +
+'outputFormat=application/json&' +
+'srsname=EPSG:4326';
+
+fetch(WATER_POLYGON)
+	.then(response => response.json())
+	.then(data => {
+        const WATER_POLYGON = L.geoJSON(data, {
+            style: function (feature) {
+                    return {
+                        fillColor: "#1a508b",
+                        fillOpacity: 0.8
+                    }},
+            onEachFeature: function (feature, layer) {
+                    layer.bindPopup(feature.properties.description);
+                    }        
+            
+        });
+		
+	    LAYERS.addOverlay(WATER_POLYGON, "Vodní plochy");
     });
 
 MAP.addControl(LAYERS);
-
 
 MAP.addLayer(OSM);
 // MAP.addLayer(HK);
 // MAP.addLayer(CIRCLE);
 
-
-MAP.panTo(HK.getLatLng());
